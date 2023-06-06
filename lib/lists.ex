2f9ab2a8015bb242666076c840c0a7c5fb7ffa81
Alessio Saltarin <alessiosaltarin@gmail.com>
2023-06-06 12:53:39 +0200

    City temperatures

--- lib/lists.ex
+++ lib/lists.ex
@@ -8,4 +8,15 @@ defmodule Lists do
   @moduledoc """
   Elixir's Lists.
   """
+
+  def random_city_temperature do
+    cities = ["Milan", "Bruxelles", "New York", "Madrid"]
+    temperatures = [15, 17, 19, 21]
+    output = []
+    for city <- cities do
+      temperature_c = "#{Enum.random(temperatures)}°C"
+      [city, temperature_c | output ]
+    end
+  end
+
 end
--- test/elixir_learn_test.exs
+++ test/elixir_learn_test.exs
@@ -15,4 +15,11 @@ defmodule ElixirLearnTest do
     Logger.debug(version)
     assert String.starts_with?(version, "ElixirLearn") == true
   end
+
+  test "Random city temperature" do
+    [ [city, temperature_c] | _tail ] = Lists.random_city_temperature()
+    Logger.debug(city <> " " <> temperature_c)
+    assert city == "Milan"
+  end
+
 end

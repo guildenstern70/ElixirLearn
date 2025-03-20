9b354c9b0713eefc60c581631621ed919888b94c
Alessio Saltarin <alessiosaltarin@gmail.com>
2025-03-20 15:51:29 +0100

    Added Anon 2

---
+++ lib/anonym_functions.ex
@@ -0,0 +1,18 @@
+#
+# ElixirLearn project
+# (C) 2023-25 Alessio Saltarin <alessiosaltarin@gmail.com>
+# MIT License
+#
+
+defmodule AnonymousFunctions do
+  @moduledoc """
+    Anonymous Functions
+  """
+  
+  def use_anonymous_functions do
+    anon = fn n -> n + 1 end
+    anon.(201) # -> 202
+  end
+  
+  
+end
